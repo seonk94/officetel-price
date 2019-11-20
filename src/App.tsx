c6df3fe76b0eead3b78fafc323284bf029192c6c
seonk1211 <seonk94@naver.com>
2019-11-20 23:06:40 +0900

    react init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
