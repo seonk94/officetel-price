c83f0cfe482ebf7367348fda217ea4100fa50bc7
seonk1211 <seonk94@naver.com>
2019-12-17 23:09:49 +0900

    color theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ResponsiveDrawer from './components/ResponsiveDrawer';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#32323B'
+    },
+    secondary: {
+      main: '#56E7A5'
+    },
+  }
+})
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <ResponsiveDrawer/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <ResponsiveDrawer/>
+      </div>
+    </ThemeProvider>
   );
 }
 
