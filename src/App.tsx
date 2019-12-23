cdf0c0ab6b6882aece3f4c1a603c6bd4d90421b7
seonk1211 <seonk94@naver.com>
2019-12-23 23:09:01 +0900

    로고 변경.

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,73 @@
-import React from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import AppBar from '../node_modules/@material-ui/core/AppBar';
-import CssBaseline from '../node_modules/@material-ui/core/CssBaseline';
-import Drawer from '../node_modules/@material-ui/core/Drawer';
-import Hidden from '../node_modules/@material-ui/core/Hidden';
-import IconButton from '../node_modules/@material-ui/core/IconButton';
-import List from '../node_modules/@material-ui/core/List';
-import ListItem from '../node_modules/@material-ui/core/ListItem';
-import ListItemIcon from '../node_modules/@material-ui/core/ListItemIcon';
-import ListItemText from '../node_modules/@material-ui/core/ListItemText';
-import HomeIcon from '../node_modules/@material-ui/icons/Home'
-import Toolbar from '../node_modules/@material-ui/core/Toolbar';
-import Typography from '../node_modules/@material-ui/core/Typography';
-import { makeStyles, useTheme, Theme, createStyles } from '../node_modules/@material-ui/core/styles';
-import Router from './Router'
-import { BrowserRouter } from 'react-router-dom';
-import bannerLogo from './assets/logos/facebook_cover_photo_1.png'
+import AppBar from "../node_modules/@material-ui/core/AppBar";
+import CssBaseline from "../node_modules/@material-ui/core/CssBaseline";
+import Drawer from "../node_modules/@material-ui/core/Drawer";
+import Hidden from "../node_modules/@material-ui/core/Hidden";
+import IconButton from "../node_modules/@material-ui/core/IconButton";
+import List from "../node_modules/@material-ui/core/List";
+import ListItem from "../node_modules/@material-ui/core/ListItem";
+import ListItemIcon from "../node_modules/@material-ui/core/ListItemIcon";
+import ListItemText from "../node_modules/@material-ui/core/ListItemText";
+import HomeIcon from "../node_modules/@material-ui/icons/Home";
+import Toolbar from "../node_modules/@material-ui/core/Toolbar";
+import Typography from "../node_modules/@material-ui/core/Typography";
+import { makeStyles, useTheme, Theme, createStyles } from "../node_modules/@material-ui/core/styles";
+import Router from "./Router";
+import { BrowserRouter } from "react-router-dom";
+import bannerLogo from "./assets/logos/facebook_cover_photo_1.png";
 
 const colorTheme = createMuiTheme({
   palette: {
     primary: {
-      main: '#393e46'
+      main: "#393e46"
     },
     secondary: {
-      main: '#56E7A5'
-    },
+      main: "#56E7A5"
+    }
   }
-})
-
+});
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
+      display: "flex"
     },
     drawer: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: drawerWidth,
-        flexShrink: 0,
-      },
+        flexShrink: 0
+      }
     },
     appBar: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-      },
+        marginLeft: drawerWidth
+      }
     },
     menuButton: {
       marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
-      },
+      [theme.breakpoints.up("sm")]: {
+        display: "none"
+      }
     },
     toolbar: theme.mixins.toolbar,
     drawerPaper: {
-      background: '#393e46',
-      width: drawerWidth,
+      background: "#393e46",
+      width: drawerWidth
     },
     content: {
       flexGrow: 1,
-      padding: theme.spacing(3),
+      padding: theme.spacing(3)
     },
     logoImg: {
-      width: '100%',
-      height: '64px',
+      width: "100%",
+      height: "64px"
     }
-  }),
+  })
 );
 
 interface ResponsiveDrawerProps {
@@ -91,14 +90,14 @@ export default function App(props: ResponsiveDrawerProps) {
   const drawer = (
     <div>
       <div className={classes.toolbar}>
-        <img src={bannerLogo} alt='logo' className={classes.logoImg} />
+        <img src={bannerLogo} alt="logo" className={classes.logoImg} />
       </div>
       <List>
-        {['/', '/chart', '/calendar'].map((text, index) => (
+        {["/"].map((text, index) => (
           <Link to={text}>
             <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <HomeIcon /> : <HomeIcon />}</ListItemIcon>
-                <ListItemText primary={text}></ListItemText>
+              <ListItemIcon>{index % 2 === 0 ? <HomeIcon /> : <HomeIcon />}</ListItemIcon>
+              <ListItemText primary={text}></ListItemText>
             </ListItem>
           </Link>
         ))}
@@ -113,18 +112,10 @@ export default function App(props: ResponsiveDrawerProps) {
           <CssBaseline />
           <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-                className={classes.menuButton}
-              >
+              <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} className={classes.menuButton}>
                 <HomeIcon />
               </IconButton>
-              <Typography variant="h6" noWrap>
-                
-              </Typography>
+              <Typography variant="h6" noWrap></Typography>
             </Toolbar>
           </AppBar>
           <nav className={classes.drawer} aria-label="mailbox folders">
@@ -133,14 +124,14 @@ export default function App(props: ResponsiveDrawerProps) {
               <Drawer
                 container={container}
                 variant="temporary"
-                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                anchor={theme.direction === "rtl" ? "right" : "left"}
                 open={mobileOpen}
                 onClose={handleDrawerToggle}
                 classes={{
-                  paper: classes.drawerPaper,
+                  paper: classes.drawerPaper
                 }}
                 ModalProps={{
-                  keepMounted: true, // Better open performance on mobile.
+                  keepMounted: true // Better open performance on mobile.
                 }}
               >
                 {drawer}
@@ -149,7 +140,7 @@ export default function App(props: ResponsiveDrawerProps) {
             <Hidden xsDown implementation="css">
               <Drawer
                 classes={{
-                  paper: classes.drawerPaper,
+                  paper: classes.drawerPaper
                 }}
                 variant="permanent"
                 open
@@ -165,7 +156,7 @@ export default function App(props: ResponsiveDrawerProps) {
         </div>
       </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 /* 
 const App: React.FC = () => {
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom';
-import TodayScreen from './Screens/TodayScreen'
-import CalendarScreen from './Screens/CalendarScreen'
-import ChartScreen from './Screens/ChartScreen'
+import * as React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import TodayScreen from "./Screens/TodayScreen";
+import CalendarScreen from "./Screens/CalendarScreen";
+import ChartScreen from "./Screens/ChartScreen";
 
-const Router : React.FC = () => (
-    <Switch>
-        <Route path="/" exact component={TodayScreen} />
-        <Route path="/calendar" component={CalendarScreen} />
-        <Route path="/chart" exact component={ChartScreen} />
-        <Redirect path="*" to="/" />
-    </Switch>
-)
+const Router: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={TodayScreen} />
+    {/* <Route path="/calendar" component={CalendarScreen} />
+        <Route path="/chart" exact component={ChartScreen} /> */}
+    <Redirect path="*" to="/" />
+  </Switch>
+);
 export default Router;
