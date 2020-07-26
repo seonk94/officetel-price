06eef952984ffb624b720cbff34c85004399def6
Kwon <seonk@taggers.io>
2020-07-26 19:02:35 +0900

    feat: enter event apply

--- src/components/NameField/index.tsx
+++ src/components/NameField/index.tsx
@@ -3,24 +3,57 @@ import styled from 'styled-components';
 
 interface NameFieldProps {
     name: string;
-    onChange: React.Dispatch<React.SetStateAction<string>>;
+    setName: React.Dispatch<React.SetStateAction<string>>;
+    searchName: () => Promise<void>;
 }
 
 const NameBox = styled.div`
     display: flex;
+    padding: 0 10px;
+    margin-top: 25vh;
+    margin-right: auto;
+    margin-left: auto;
+    align-items: center;
+    
+    border: 1px solid black;
+    border-radius: 4px;
     width: 30%;
-    margin: auto;
+    height: fit-content;
 
     input {
         width: 100%;
         height: 36px;
+        font-size: 20px;
+        border: none;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    i {
+        font-size: 26px;
+
+        &:hover {
+            cursor : pointer;
+            color: #ddd;
+        }
     }
 `
 
-function NameField({ name, onChange }: NameFieldProps) {
+function NameField({ name, setName, searchName }: NameFieldProps) {
+    const handleKeyPress = (e : React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            searchName()
+        }
+    }
     return (
         <NameBox>
-            <input onChange={({ target: { value } }) => onChange(value)}/>
+            <input 
+                onChange={({ target: { value } }) => setName(value)}
+                onKeyPress={handleKeyPress}
+                />
+            <i className="fas fa-search" onClick={searchName}></i>
         </NameBox>
     )
 }
--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import NameField from '../components/NameField';
 import styled from 'styled-components';
+import { agify, genderize, nationalize } from '../api';
+import { IAgify, IGenderize, INationalize } from '../types/api';
 
 const MainSection = styled.section`
     width: 100%;
@@ -11,12 +13,30 @@ const MainSection = styled.section`
 function Main() {
 
     const [name, setName] = useState('');
+    const [age, setAge] = useState<IAgify>();
+    const [gender, setGender] = useState<IGenderize>();
+    const [nation, setNation] = useState<INationalize>();
 
+    const getNameInfo = async() => {
+        const [age, gender, nation] = await Promise.all([
+            agify(name),
+            genderize(name),
+            nationalize(name),
+        ]);
+
+        if (age) setAge(age);
+        if (gender) setGender(gender);
+        if (nation) setNation(nation);
+    }
     return (
         <MainSection>
             <NameField 
                 name={name}
-                onChange={setName}/>
+                setName={setName}
+                searchName={getNameInfo}/>
+            <h3>{age ? age.age : ''}</h3>
+            <h3>{gender ? gender.gender : ''}</h3>
+            <h3>{nation ? nation.country.map(c => c.country_id).join(',') : ''}</h3>
         </MainSection>
     )
 }
