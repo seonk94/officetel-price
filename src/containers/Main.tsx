3ce7eb18811bce3d1f44c217e0a72d98b1274648
seonk94 <seonk94@naver.com>
2020-07-27 21:54:23 +0900

    feat: Loader, ResultForm add

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,43 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NameField from '../components/NameField';
 import styled from 'styled-components';
 import { agify, genderize, nationalize } from '../api';
 import { IAgify, IGenderize, INationalize } from '../types/api';
+import Loader from '../components/Loader';
+import ResultForm from '../components/ResultForm';
+import { LoadingStatus } from '../types';
 
 const MainSection = styled.section`
     width: 100%;
     height: 100vh;
     display: grid;
-
+    grid-template-rows: 40% 60%;
 `
 function Main() {
 
-    const [name, setName] = useState('');
-    const [age, setAge] = useState<IAgify>();
-    const [gender, setGender] = useState<IGenderize>();
-    const [nation, setNation] = useState<INationalize>();
+  const [name, setName] = useState('');
+  const [age, setAge] = useState<IAgify>();
+  const [gender, setGender] = useState<IGenderize>();
+  const [nation, setNation] = useState<INationalize>();
+  const [loadingStatus, setLoadingStatus] = useState<LoadingStatus>('waiting');
 
-    const getNameInfo = async() => {
-        const [age, gender, nation] = await Promise.all([
-            agify(name),
-            genderize(name),
-            nationalize(name),
-        ]);
+  useEffect(() => {
+    setLoadingStatus('waiting')
+  }, [name])
 
-        if (age) setAge(age);
-        if (gender) setGender(gender);
-        if (nation) setNation(nation);
-    }
-    return (
-        <MainSection>
-            <NameField 
-                name={name}
-                setName={setName}
-                searchName={getNameInfo}/>
-            <h3>{age ? age.age : ''}</h3>
-            <h3>{gender ? gender.gender : ''}</h3>
-            <h3>{nation ? nation.country.map(c => c.country_id).join(',') : ''}</h3>
-        </MainSection>
-    )
+  const getNameInfo = async () => {
+    setLoadingStatus('searching')
+    const [age, gender, nation] = await Promise.all([
+      agify(name),
+      genderize(name),
+      nationalize(name),
+    ]);
+
+    if (age) setAge(age);
+    if (gender) setGender(gender);
+    if (nation) setNation(nation);
+    setLoadingStatus('searched')
+  }
+  return (
+    <MainSection>
+      <NameField
+        name={name}
+        setName={setName}
+        searchName={getNameInfo} />
+      <ResultForm
+        age={age}
+        gender={gender}
+        nation={nation}
+        loadingStatus={loadingStatus}
+      />
+    </MainSection>
+  )
 }
 export default Main;
