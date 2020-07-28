25022abc43e5bf3bdfed3608e62d51be36c7861f
seonk94 <seonk94@naver.com>
2020-07-28 21:16:06 +0900

    feat: bounce keyframes

---
+++ src/style/easing/index.ts
@@ -0,0 +1,169 @@
+import { keyframes } from 'styled-components';
+
+export const BounseLeftReturn = keyframes`
+  0% {
+    transform: translateX(-100%);
+  }
+
+  12% {
+    transform: translateX(-89.11%);
+  }
+
+  24% {
+    transform: translateX(-56.44%);
+  }
+
+  36% {
+    transform: translateX(-1.99%);
+  }
+
+  54% {
+    transform: translateX(-24.98%);
+  }
+
+  74% {
+    transform: translateX(-1.63%);
+  }
+
+  82% {
+    transform: translateX(-6.25%);
+  }
+
+  92% {
+    transform: translateX(-0.66%);
+  }
+
+  96% {
+    transform: translateX(-1.54%);
+  }
+
+  100% {
+    transform: translateX(0%);
+  }
+`
+
+export const BounceRightReturn = keyframes`
+  0% {
+    transform: translateX(100%);
+  }
+
+  12% {
+    transform: translateX(89.11%);
+  }
+
+  24% {
+    transform: translateX(56.44%);
+  }
+
+  36% {
+    transform: translateX(1.99%);
+  }
+
+  54% {
+    transform: translateX(24.98%);
+  }
+
+  74% {
+    transform: translateX(1.64%);
+  }
+
+  82% {
+    transform: translateX(6.25%);
+  }
+
+  92% {
+    transform: translateX(0.66%);
+  }
+
+  96% {
+    transform: translateX(1.54%);
+  }
+
+  100% {
+    transform: translateX(0%);
+  }
+`
+
+
+export const BounseTopReturn = keyframes`
+  0% {
+    transform: translateY(-100%);
+  }
+
+  12% {
+    transform: translateY(-89.11%);
+  }
+
+  24% {
+    transform: translateY(-56.44%);
+  }
+
+  36% {
+    transform: translateY(-1.99%);
+  }
+
+  54% {
+    transform: translateY(-24.98%);
+  }
+
+  74% {
+    transform: translateY(-1.63%);
+  }
+
+  82% {
+    transform: translateY(-6.25%);
+  }
+
+  92% {
+    transform: translateY(-0.66%);
+  }
+
+  96% {
+    transform: translateY(-1.54%);
+  }
+
+  100% {
+    transform: translateY( 0%);
+  }
+`
+export const BounseBottomReturn = keyframes`
+  0% {
+    transform: translateY(100%);
+  }
+
+  12% {
+    transform: translateY(89.11%);
+  }
+
+  24% {
+    transform: translateY(56.44%);
+  }
+
+  36% {
+    transform: translateY(1.99%);
+  }
+
+  54% {
+    transform: translateY(24.98%);
+  }
+
+  74% {
+    transform: translateY(1.64%);
+  }
+
+  82% {
+    transform: translateY(6.25%);
+  }
+
+  92% {
+    transform: translateY(0.66%);
+  }
+
+  96% {
+    transform: translateY(1.54%);
+  }
+
+  100% {
+    transform: translateY(0%);
+  }
+`
