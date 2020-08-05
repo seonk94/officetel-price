e0627a0aed4fa5c3a69ab102527587ce832701b2
seonk94 <seonk94@naver.com>
2020-08-05 22:39:10 +0900

    delete: 사용안하는 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_sidebar_responsive
-https://jsramblings.com/2018/02/04/styled-components-media-queries.html
-https://velog.io/@velopert/create-your-own-design-system-with-storybook
-https://emotion.sh/docs/media-queries#gatsby-focus-wrapper
-
-*/
