dc0165fdec66a94ac1f42e067b88d68fe85aeafb
seonk94 <seonk94@naver.com>
2020-08-13 21:09:43 +0900

    feat: lightTheme, darkTheme style init

---
+++ src/style/theme/index.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  colors: {
+    mainTextColor: '#000',
+    subTextColor: '#fff',
+  }
+}
+
+export const darkTheme = {
+  colors: {
+    mainTextColor: '#fff',
+    subTextColor: '#000'
+  }
+}
