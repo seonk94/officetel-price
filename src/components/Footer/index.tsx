e73f84a4b3429ec9ded497756e808323ea00cee2
seonk94 <seonk94@naver.com>
2020-08-13 21:17:29 +0900

    feat: Footer component Theme apply

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { FooterColor } from '@/src/constants/Colors';
+import styled, { ThemeProvider } from 'styled-components';
+import { useTheme } from '@/src/hooks/useTheme';
 
 const FooterDiv = styled.div`
   display: grid;
-  background: ${FooterColor};
+  background: ${props => props.theme.colors.subBgColor};
   grid-template-columns: 1fr 1fr;
   align-items: center;
   padding: 0px 12px;
-  color: #fff;
+  color: ${props => props.theme.colors.subTextColor};
   font-weight: 700;
   font-family: 'Work Sans', sans-serif;
 `
@@ -21,22 +21,27 @@ const GithubDiv = styled.div`
   text-align: right;
 `
 const GithubAnchor = styled.a`
-  color: #fff;
+  color: ${props => props.theme.colors.subTextColor};
   text-decoration: none;
 `
 
 function Footer() {
+
+  const [theme] = useTheme();
+
   return (
-    <FooterDiv>
-      <BubaDiv>
-        Buba ToyProject
+    <ThemeProvider theme={theme}>
+      <FooterDiv>
+        <BubaDiv>
+          Buba ToyProject
       </BubaDiv>
-      <GithubDiv>
-        <GithubAnchor href="https://github.com/seonk94/react" target="_blank">
-          Github
+        <GithubDiv>
+          <GithubAnchor href="https://github.com/seonk94/react" target="_blank">
+            Github
         </GithubAnchor>
-      </GithubDiv>
-    </FooterDiv>
+        </GithubDiv>
+      </FooterDiv>
+    </ThemeProvider>
   )
 }
 export default Footer
