320b4c2128ee7e96d7d2a219f6f92aad922b1d53
seonk94 <seonk94@naver.com>
2020-08-13 21:18:06 +0900

    feat: Apply Theme PageTitle Component

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { FillOffset, StrokeOffset } from '@/src/style/animation';
+import { useTheme } from '@/src/hooks/useTheme';
 
 const TitleDiv = styled.div`
   height: 200px;
@@ -11,7 +12,7 @@ const TitleSvg = styled.svg<{ render: boolean }>`
   font-family: 'Indie Flower', cursive;
 
   .stroke {
-    stroke: #000;
+    stroke: ${props => props.theme.colors.mainTextColor};
     stroke-width: 1px;
     stroke-dasharray: 250 250;
     stroke-opacity 1;
@@ -24,7 +25,7 @@ const TitleSvg = styled.svg<{ render: boolean }>`
   }
 
   .fill {
-    fill: #000;
+    fill: ${props => props.theme.colors.mainTextColor};
     fill-opacity: 1;
     animation: ${FillOffset} 2s ease alternate 1;
     animation-timing-function: cubic-bezier(.25, .46, .45, .94)
@@ -32,23 +33,26 @@ const TitleSvg = styled.svg<{ render: boolean }>`
 `
 function PageTitle() {
 
+  const [theme] = useTheme();
   const [render, setRender] = useState(false);
   useEffect(() => {
     setRender(true);
   }, [])
 
   return (
-    <TitleDiv>
-      <TitleSvg version="1.1" viewBox="0 0 300 100" render={render}>
-        <symbol id="fade-text">
-          <text x="50%" y="50%" textAnchor="middle">Who are you</text>
-        </symbol>
-        <g>
-          <use className="stroke" xlinkHref="#fade-text" />
-          <use className="fill" xlinkHref="#fade-text" />
-        </g>
-      </TitleSvg>
-    </TitleDiv>
+    <ThemeProvider theme={theme}>
+      <TitleDiv>
+        <TitleSvg version="1.1" viewBox="0 0 300 100" render={render}>
+          <symbol id="fade-text">
+            <text x="50%" y="50%" textAnchor="middle">Who are you</text>
+          </symbol>
+          <g>
+            <use className="stroke" xlinkHref="#fade-text" />
+            <use className="fill" xlinkHref="#fade-text" />
+          </g>
+        </TitleSvg>
+      </TitleDiv>
+    </ThemeProvider>
   )
 }
 export default PageTitle;
