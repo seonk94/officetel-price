bc9c8c3a3a44940bcfee2a4b0607365ebb313a43
seonk94 <seonk94@naver.com>
2020-08-13 21:17:11 +0900

    update: theme color update

--- src/style/theme/index.ts
+++ src/style/theme/index.ts
@@ -2,12 +2,16 @@ export const lightTheme = {
   colors: {
     mainTextColor: '#000',
     subTextColor: '#fff',
+    mainBgColor: '#fff',
+    subBgColor: '#000',
   }
 }
 
 export const darkTheme = {
   colors: {
     mainTextColor: '#fff',
-    subTextColor: '#000'
+    subTextColor: '#000',
+    mainBgColor: '#000',
+    subBgColor: '#fff',
   }
 }
