cd14376e3e8c7b58e81fc2d5f1c5c34f9ed449a7
seonk94 <seonk94@naver.com>
2020-08-14 20:53:43 +0900

    feat: CustomTheme interface init

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,3 +1,12 @@
 export type LoadingStatus = 'searching' | 'waiting' | 'searched';
 
 export type ThemeMode = 'dark' | 'light'
+
+export interface CustomTheme {
+  colors: {
+    mainTextColor: string;
+    subTextColor: string;
+    mainBgColor: string;
+    subBgColor: string;
+  }
+}
